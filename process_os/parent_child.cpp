ebf1d015eac17b937358adf89eccd0c1dd536714
heoraengi <heo.raeng.i@gmail.com>
2024-02-05 16:24:36 +0900

    parent, child process v0.1

--- process_os/parent_child.cpp
+++ process_os/parent_child.cpp
@@ -1,9 +1,20 @@
+// 자식 프로세스 출력하기
 #include<iostream>
 #include<unistd.h>
 // unistd.h는 운영체제 API에 대한 액세스를 제공함
 using namespace std;
 
 int main(){
-    cout << "pid : " << getpid() << endl;
+    cout << "부모 pid : " << getpid() << endl;
+    if (fork()==0){
+        // fork()는 자식 프로세스를 생성해주는 명령어
+        // fork()는 부모 프로세스이면 자식의 pid값
+        // 자식 프로세스이면 0을 반환함
+        // 실행이 실패하면 -1을 반환
+        cout << "자식 pid : " << getpid() << endl;  
+        // fork()가 0이므로 자식 pid 출력
+
+    }
     return 0;
 }
+
