803138bb879e856068b13a9542ab64c67427b3a8
heoraengi <heo.raeng.i@gmail.com>
2024-02-05 16:27:51 +0900

    parent, child process v0.2

--- process_os/parent_child.cpp
+++ process_os/parent_child.cpp
@@ -7,14 +7,9 @@ using namespace std;
 int main(){
     cout << "부모 pid : " << getpid() << endl;
     if (fork()==0){
-        // fork()는 자식 프로세스를 생성해주는 명령어
-        // fork()는 부모 프로세스이면 자식의 pid값
-        // 자식 프로세스이면 0을 반환함
-        // 실행이 실패하면 -1을 반환
         cout << "자식 pid : " << getpid() << endl;  
-        // fork()가 0이므로 자식 pid 출력
-
     }
+    cout << getpid() << " : 실행합니다" << endl;
     return 0;
 }
 
